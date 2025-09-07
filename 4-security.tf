fee19220fa00e7525989725bb2aaa599ae42f356
tony-waters <2tonywaters@gmail.com>
2025-09-07 03:17:47 +0100

    get rid of aws_security_group_rules

--- 4-security.tf
+++ 4-security.tf
@@ -5,30 +5,44 @@
 
 resource "aws_security_group" "ec2_public" {
   name        = "ec2-public"
-  description = "Allow SSH to public server"
+  description = "Allow SSH to public server and egress"
   vpc_id      = aws_vpc.main.id
+
+  ingress {
+    from_port         = 22
+    to_port           = 22
+    protocol          = "tcp"
+    cidr_blocks       = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port         = 0
+    to_port           = 0
+    protocol          = "-1"
+    cidr_blocks       = ["0.0.0.0/0"]
+  }
 }
 
 #
 # security rules
 #
 
-resource "aws_security_group_rule" "allow_ssh" {
-  type              = "ingress"
-  description       = "SSH ingress"
-  from_port         = 22
-  to_port           = 22
-  protocol          = "tcp"
-  cidr_blocks       = ["0.0.0.0/0"]
-  security_group_id = aws_security_group.ec2_public.id
-}
-
-# resource "aws_security_group_rule" "allow_ping" {
+# resource "aws_security_group_rule" "allow_ssh" {
 #   type              = "ingress"
-#   description       = "ICMP ingress"
-#   from_port         = 8
+#   description       = "SSH ingress"
+#   from_port         = 22
+#   to_port           = 22
+#   protocol          = "tcp"
+#   cidr_blocks       = ["0.0.0.0/0"]
+#   security_group_id = aws_security_group.ec2_public.id
+# }
+#
+# resource "aws_security_group_rule" "allow_egress" {
+#   type              = "egress"
+#   description       = "allow egress"
+#   from_port         = 0
 #   to_port           = 0
-#   protocol          = "icmp"
+#   protocol          = "-1"
 #   cidr_blocks       = ["0.0.0.0/0"]
 #   security_group_id = aws_security_group.ec2_public.id
 # }
